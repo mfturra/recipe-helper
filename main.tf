312d6c7b6993f6496010605b198e66d62a053029
Matheus Turra <turra.m@northeastern.edu>
2023-09-06 18:11:58 -0400

    Formatting and updates were made to validate tf workflow

--- main.tf
+++ main.tf
@@ -9,24 +9,15 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-region {
-    default     = "us-east-2"
-    description = "AWS region"
-}
 
 provider "aws" {
-  region  = "us-east-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "gpt_build" {
     instance_type       = "t2.micro"
     ami                 = "ami-053b0d53c279acc90"
-    security_groups     = ["aws_security_groups.flask_web_server_sec_group.name"]
-
-    tags = {
-      name              = "gpt_build"
-    }
-    //key_name = "vs-flask-1"
+    
     
     # root disk
     root_block_device {
@@ -49,15 +40,15 @@ resource "tls_private_key" "RSA_key" {
 
 # Store Private Key Locally
 resource "local_file" "RSA_key" {
-  content = tls_private_key.RSA_key.private_key_pem
-  filename = "tfkey"
+  content   = tls_private_key.RSA_key.private_key_pem
+  filename  = "tfkey"
 }
 
 # Security group config
 resource "aws_security_group" "flask_web_server_sec_group" {
     name                = "flask_web_server_sec_group"
     description         = "Security group for Terraform build"
-    vpc_id              = aws_vpc.gpt_build
+    vpc_id              = aws_vpc.gpt_build_vpc
 
     ingress {
       description     = "SSH"
@@ -97,50 +88,46 @@ resource "aws_security_group" "flask_web_server_sec_group" {
       protocol        = "-1"
       cidr_blocks     = ["0.0.0.0/0"]
     }
-
-    tags {
-      Name = "flask_web_server_sec_group"
-    }
-
 }
 
 # AWS VPC Config
 resource "aws_vpc" "gpt_build_vpc" {
-  name = "gpt_build-vpc"
-  cidr_block = "10.0.0.0/16"
+  cidr_block  = "10.0.0.0/16"
 
   tags = {
-    Name = "TerraformVPC"
+    Name      = "TerraformVPC"
   }
 }
 
 # Public Subnet Config
 resource "aws_subnet" "PublicSubnet" {
-  vpc_id = aws_vpc.gpt_build_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id      = aws_vpc.gpt_build_vpc.id
+  cidr_block  = "10.0.1.0/24"
 }
 
 # Private Subnet Config
 resource "aws_subnet" "PrivateSubnet" {
-  vpc_id = aws_vpc.gpt_build_vpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id      = aws_vpc.gpt_build_vpc.id
+  cidr_block  = "10.0.2.0/24"
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.gpt_build_vpc.id
+  vpc_id      = aws_vpc.gpt_build_vpc.id
 }
 
 # Route table for Public Subnet
 resource "aws_route_table" "PublicRT" {
   vpc_id = aws_vpc.gpt_build_vpc.id
-  route = {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
 }
 
+resource "aws_route" "PublicRoute" {
+  route_table_id          = aws_route_table.PublicRT.id
+  destination_cidr_block  = "0.0.0.0/0"
+  gateway_id              = aws_internet_gateway.igw.id
+  }
+
 # Route table association public subnet
-resource "aws_route_table_associate" "PublicRTAssociation" {
-  subnet_id = aws_subnet.PublicSubnet.id
-  route_table_id = aws_route_table.PublicRT.id
+resource "aws_route_table_association" "PublicRTAssociation" {
+  subnet_id       = aws_subnet.PublicSubnet.id
+  route_table_id  = aws_route_table.PublicRT.id
 }
